96e135ee6e42c14f7b6cc3526b480fbe1c35964b
Rodrigo Isidro <isidro.rodrigo@gmail.com>
2022-02-06 22:23:00 -0300

    Introduction to view

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function SobreNos() {
-        echo 'Sobre-nós!';
+        return view('site.sobre-nos');
     }
 }
